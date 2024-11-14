a957afb2f43e21f5b0f001bfbe594f78a2280e08
cesardmn <cesardmn@gmail.com>
2024-11-14 16:40:17 -0300

    refactor: Alterando o xlsx para usar async/await.

--- smt/js/xlsx.js
+++ smt/js/xlsx.js
@@ -1,35 +1,21 @@
 export const XlsxProcessos = (() => {
   const readerXLSX = async (file) => {
-    return new Promise((resolve) => {
-      const reader = new FileReader()
-
-      reader.onload = () => {
-        try {
-          const data = reader.result
-          const workbook = XLSX.read(new Uint8Array(data), { type: 'array' })
-          const sheet = workbook.Sheets[workbook.SheetNames[0]]
-          const json = XLSX.utils.sheet_to_json(sheet, { defval: '' })
-          resolve({
-            status: 'ok',
-            data: json,
-          })
-        } catch (error) {
-          resolve({
-            status: 'nok',
-            erro: `Erro ao processar o arquivo XLSX: ${error.message}`,
-          })
-        }
+    try {
+      const arrayBuffer = await file.arrayBuffer()
+      const workbook = XLSX.read(arrayBuffer, { type: 'array' })
+      const sheetName = workbook.SheetNames[0]
+      const sheet = workbook.Sheets[sheetName]
+      const data = XLSX.utils.sheet_to_json(sheet, { defval: '' })
+      return {
+        status: 'ok',
+        data,
       }
-
-      reader.onerror = (error) => {
-        resolve({
-          status: 'nok',
-          erro: `Erro ao ler o arquivo: ${error.message}`,
-        })
+    } catch (error) {
+      return {
+        status: 'nok',
+        error,
       }
-
-      reader.readAsArrayBuffer(file)
-    })
+    }
   }
 
   return {
